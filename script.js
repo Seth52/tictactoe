4c711b44db0cdf20365fd4c92ad4393bddd39ba7
Seth <25378370+Seth52@users.noreply.github.com>
2022-10-20 18:24:51 -0400

    gameboard regex

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const gameboard = [
-['','',''],
-['','',''],
-['','','']
+['x','x','x'],
+['x','x','x'],
+['x','x','x']
 
 
 ];
-console.log(gameboard.toString());
+//divs with borders only on certain spots?
+console.log(`${gameboard.join('\n')}`);
+
 
 let test = document.getElementById('body');
-let y = document.createTextNode(gameboard);
+let y = document.createTextNode(`${gameboard.join('\n')}\n\n`);
 test.append(y);
 
 /*const players =[
     [player1]
     [player2]
 ]*/ 
+
+const board = [
+    ['R','N','B','Q','K','B','N','R'],
+    ['P','P','P','P','P','P','P','P'],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    [' ',' ',' ',' ',' ',' ',' ',' '],
+    ['p','p','p','p','p','p','p','p'],
+    ['r','n','b','q','k','b','n','r'] ];
+  
+  console.log(`${board.join('\n')}\n\n`);
