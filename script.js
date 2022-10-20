13319b50010de5f617e563d910ea6911fe1cf20a
Seth <25378370+Seth52@users.noreply.github.com>
2022-10-20 18:17:08 -0400

    test

---
+++ script.js
@@ -0,0 +1,17 @@
+const gameboard = [
+['','',''],
+['','',''],
+['','','']
+
+
+];
+console.log(gameboard.toString());
+
+let test = document.getElementById('body');
+let y = document.createTextNode(gameboard);
+test.append(y);
+
+/*const players =[
+    [player1]
+    [player2]
+]*/ 
