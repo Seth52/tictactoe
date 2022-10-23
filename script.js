1fc0c862b3cf2b427c6a25609406425e9def351a
Seth <25378370+Seth52@users.noreply.github.com>
2022-10-22 21:05:57 -0400

    daY2

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
-const gameboard = [
-['x','x','x'],
-['x','x','x'],
-['x','x','x']
 
 
-];
+const gameboardModule =(() =>{
+    
+    const gameboard = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+      
+               
+        ];
+    for(let i=0; i<gameboard.length; i++){
+        console.log(gameboard[i]);
+    }
+
+})();
+
+let xo = document.getElementById('container');
+xo.addEventListener('click',function(e){
+    if(e==='')
+})
+
+
 //divs with borders only on certain spots?
-console.log(`${gameboard.join('\n')}`);
+//console.log(`${gameboard.join('\n')}`);
 
 
-let test = document.getElementById('body');
-let y = document.createTextNode(`${gameboard.join('\n')}\n\n`);
-test.append(y);
 
-/*const players =[
-    [player1]
-    [player2]
-]*/ 
+/*let test = document.getElementById('body');
+let y = document.createTextNode(`${gameboard.join('\n')}`);
+test.append(y);*/
 
-const board = [
-    ['R','N','B','Q','K','B','N','R'],
-    ['P','P','P','P','P','P','P','P'],
-    [' ',' ',' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ',' ',' '],
-    [' ',' ',' ',' ',' ',' ',' ',' '],
-    ['p','p','p','p','p','p','p','p'],
-    ['r','n','b','q','k','b','n','r'] ];
+function createPlayer(name){
+    name
+    
+
+};
+
+
+
+  //factory function for players 
   
-  console.log(`${board.join('\n')}\n\n`);
+  
+  // create an array gameboard where we can give an id to create the css for the gameboard? 
+  //for targeting the dom array locations will be where x/o will be written
